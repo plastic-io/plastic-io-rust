0c83ad736a38945dd5e261447e422b51bf8a90b6
Tony Germaneri <Tony.Germaneri@gmail.com>
2024-03-01 01:46:48 -0800

    added file loader to cli

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ mod utils;
 mod loader;
 mod types;
 use clap::Parser;
+use crate::types::Graph;
 
 // Graph parser
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Graph path
+    #[arg(short, long, default_value = "")]
+    path: String,
     /// Graph JSON
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "")]
     graph: String,
     /// Node URL Entrypoint
     #[arg(short, long, default_value = "main")]
@@ -24,22 +28,23 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    match loader::parse_graph(&args.graph) {
-        Ok(graph) => {
-            let scheduler = types::Scheduler::new(graph, None);
-            scheduler
-            .event_emitter
-            .subscribe(types::EventType::AfterSet, |event| {
-                let data = &event.data;
-                    if let Some(return_value) = data.get("return") {
-                        print!("{}", return_value);
-                    }
-            });
-            scheduler.url(args.url, serde_json::Value::String(args.value.to_string()), args.field);
-        },
-        Err(e) => {
-            eprintln!("Error parsing JSON into Graph: {}", e);
-            std::process::exit(1);
-        }
-    }
+    let graph: Graph = if &args.graph.len() > &0 {
+        loader::parse_graph(&args.graph).expect("Error parsing graph JSON")
+    } else if &args.path.len() > &0 {
+        loader::load_graph_from_file(&args.path)
+    } else {
+        println!("No graph source defined.  You must define either --path or --graph.");
+        return;
+    };
+
+    let scheduler = types::Scheduler::new(graph, None);
+    scheduler
+    .event_emitter
+    .subscribe(types::EventType::AfterSet, |event| {
+        let data = &event.data;
+            if let Some(return_value) = data.get("return") {
+                print!("{}", return_value);
+            }
+    });
+    scheduler.url(args.url, serde_json::Value::String(args.value.to_string()), args.field);
 }
